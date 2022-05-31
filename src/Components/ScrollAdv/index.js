01c296adc07cbe0e67e9aa8dd8a4a7fd280972a8
Ruben Gonzalez <rubengon.uk@gmail.com>
2022-05-31 11:26:43 +0100

    funcionality added to the scroll buttons

--- src/Components/ScrollAdv/index.js
+++ src/Components/ScrollAdv/index.js
@@ -4,9 +4,14 @@ import "./scrollAdv.css";
 // Component for showing the option of scrolling down or up on the right side of the screen.
 function ScrollAdv() {
 
+  console.log(window.innerHeight);
+
   return (
     <div
       className="scrollContainer"
+      onClick={() => {
+        window.scrollTo({ top: (window.innerHeight * 2.1), behavior: "smooth" });
+      }}
     >
       <span className="scrollSpan">Projects</span>
       <div className="arrowsDown">
